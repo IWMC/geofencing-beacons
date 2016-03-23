d1acc1f050d3f11287e49e49797f9a40aacc1fd2
Brent Couck <Brent.Couck@realdolmen.com>
2016-03-23 11:18:23 +0100

    Removed unnecessary 'created by' comments

--- Android/TimeRegistration/app/src/main/java/com/realdolmen/timeregistration/service/GenericVolleyError.java
+++ Android/TimeRegistration/app/src/main/java/com/realdolmen/timeregistration/service/GenericVolleyError.java
@@ -2,9 +2,6 @@ package com.realdolmen.timeregistration.service;
 
 import com.android.volley.VolleyError;
 
-/**
- * Created by Brent on 29/02/2016.
- */
 public class GenericVolleyError extends VolleyError {
 	public GenericVolleyError(String exceptionMessage) {
 		super(exceptionMessage);
--- Android/TimeRegistration/app/src/main/java/com/realdolmen/timeregistration/service/repository/LoadCallback.java
+++ Android/TimeRegistration/app/src/main/java/com/realdolmen/timeregistration/service/repository/LoadCallback.java
@@ -1,8 +1,5 @@
 package com.realdolmen.timeregistration.service.repository;
 
-/**
- * Created by BCCAZ45 on 21/03/2016.
- */
 public interface LoadCallback {
 	enum Result {
 		SUCCESS, FAIL
